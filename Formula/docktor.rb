972fbebb4d2d12e9ef05204b7356d50b2305fa39
Sargis Sargsyan <s.a.sargsyan@tbank.ru>
2026-06-04 22:23:31 +0400

    Fix arm64 checksum

--- Formula/docktor.rb
+++ Formula/docktor.rb
@@ -4,12 +4,12 @@ class Docktor < Formula
   version "1.0.1"
 
   if Hardware::CPU.arm?
-    url "https://github.com/fullstack-sargis/docktor/releases/download/v1.0.1/docktor-v1.0.1-macos-arm64.tar.gz"
-    sha256 "4718a0cd3791baa5272e50f46e6f6dcfd3a8606aac535426d544ac0560bc0560"
-  else
-    url "https://github.com/fullstack-sargis/docktor/releases/download/v1.0.1/docktor-v1.0.1-macos-x64.tar.gz"
-    sha256 "4f3c3c37b9b1c778b355688c0305ed480812b30116fffec90e594921662b05c8"
-  end
+  url "https://github.com/fullstack-sargis/docktor/releases/download/v1.0.1/docktor-v1.0.1-macos-arm64.tar.gz"
+  sha256 "30a8c2967742cdc39df61eb65ce4786c3ac43056417991e163ca49c531e93472"
+else
+  url "https://github.com/fullstack-sargis/docktor/releases/download/v1.0.1/docktor-v1.0.1-macos-x64.tar.gz"
+  sha256 "4f3c3c37b9b1c778b355688c0305ed480812b30116fffec90e594921662b05c8"
+end
 
   def install
     bin.install "docktor"
